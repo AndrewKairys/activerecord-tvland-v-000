aea874a151c9a3e7cfe7630f49ffa5226413575c
Andrew Kairys <andrewkairys@gmail.com>
2018-06-12 15:39:40 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 9) do
+=======
+ActiveRecord::Schema.define(version: 4) do
+>>>>>>> 8c36b2029dcb7da6f6a2deb668cf28da89ca4dd3
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
   end
 
+<<<<<<< HEAD
   create_table "characters", force: :cascade do |t|
     t.string "name"
     t.integer "show_id"
@@ -24,6 +29,8 @@ ActiveRecord::Schema.define(version: 9) do
     t.string "catchphrase"
   end
 
+=======
+>>>>>>> 8c36b2029dcb7da6f6a2deb668cf28da89ca4dd3
   create_table "networks", force: :cascade do |t|
     t.string "call_letters"
     t.integer "channel"
@@ -32,9 +39,12 @@ ActiveRecord::Schema.define(version: 9) do
   create_table "shows", force: :cascade do |t|
     t.string "name"
     t.integer "network_id"
+<<<<<<< HEAD
     t.string "genre"
     t.string "day"
     t.string "season"
+=======
+>>>>>>> 8c36b2029dcb7da6f6a2deb668cf28da89ca4dd3
   end
 
 end
